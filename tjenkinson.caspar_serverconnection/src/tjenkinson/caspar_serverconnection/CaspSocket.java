6ac90be724056c14715710fafbcf618b522550ad
Tom Jenkinson <tjcode@outlook.com>
2013-04-01 00:22:14 +0100

    Removed debug print lines and unused variables and imports.

--- tjenkinson.caspar_serverconnection/src/tjenkinson/caspar_serverconnection/CaspSocket.java
+++ tjenkinson.caspar_serverconnection/src/tjenkinson/caspar_serverconnection/CaspSocket.java
@@ -3,7 +3,6 @@ package tjenkinson.caspar_serverconnection;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Date;
@@ -16,18 +15,15 @@ import tjenkinson.caspar_serverconnection.commands.CaspReturn;
 public class CaspSocket {
 	
 	private Socket socket = null;
-	private PrintWriter out = null;
 	private BufferedReader in = null;
 	
 	public CaspSocket(String caspAddress, int caspPort) throws IOException {
 		socket = new Socket(InetAddress.getByName(caspAddress), caspPort);
-		out = new PrintWriter(socket.getOutputStream(), true);
 		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 	}
 	
 	
 	public synchronized CaspReturn runCmd(CaspCmd cmd) throws IOException {
-		out.println(cmd.getCmdString());
 		long requestTime = new Date().getTime();
 		
 		String line = in.readLine();
